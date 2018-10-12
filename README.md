0ee49ffef113aa19fe83b8d09aac29f6ac2ca27a
lxx.imac <lxx2013@mail.ustc.edu.cn>
2018-10-12 22:17:31 +0800

    配置列出文件列表成功,但还不完善,后续要继续从原生前端 File 入口按顺序理清楚

--- README.md
+++ README.md
@@ -16,7 +16,32 @@
   - [ ] 七牛云
   - [ ] 针对三家图床API(包括自己)使用图标颜色来标注 `宕机` or `正常服务中`
 
+## 总结
+#### 1.FileList 对象获取
+目前只能通过`表单选择文件`或者`拖拽文件`或者`剪切板事件`
+```js
+document.querySelector('input').onchange = function() {
+  console.log(this.files);
+};
+//或者拖拽事件
+var ipt = document.querySelector('textarea');
+ipt.ondragover = function () { return false; };
+ipt.ondrop = function(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  e = e || window.event;
+  var files = e.dataTransfer.files;
+  console.log(files);
+};
+```
+#### 2. FileList 中的 File 对象属性
+- `name`：文件名，该属性只读。
+- `size`：文件大小，单位为字节，该属性只读。
+- `type`：文件的 MIME 类型，如果分辨不出类型，则为空字符串，该属性只读。
+- `lastModified`：文件的上次修改时间，格式为时间戳。
+- `lastModifiedDate`：文件的上次修改时间，格式为 Date 对象实例。
 
 ## 参考文档
 - [鑫空间/安兹·乌尔·恭的超位魔法-任意拖拽](https://www.zhangxinxu.com/wordpress/2018/09/drag-drop-datatransfer-js/)
 - [掘金 Koa2文件上传下载](https://juejin.im/post/5abc451ff265da23a2292dd4)
+- [HTML5 File API — 让前端操作文件变的可能](https://www.cnblogs.com/zichi/p/html5-file-api.html)
--- i-setsuna-wang.js
+++ i-setsuna-wang.js
@@ -1,1 +1,45 @@
 /* 静态资源站点 */
+const Koa = require('koa')
+const fs = require('fs')
+const path = require('path')
+const app = new Koa()
+const Router = require('koa-router')
+const serve = require('koa-static')
+const staticUtil = require('./js/staticUtil')
+
+var router = new Router()
+// 静态资源服务器
+//router.get('/static', async (ctx) => {
+router.get('/', async (ctx) => {
+    // 静态资源目录在本地的绝对路径
+    let fullStaticPath = path.join(__dirname, './')
+
+    // 获取静态资源内容，有可能是文件内容，目录，或404
+    let _content = await staticUtil.content(ctx, fullStaticPath)
+
+    // 解析请求内容的类型
+    let _mime = staticUtil.parseMime(ctx.url)
+
+    // 如果有对应的文件类型，就配置上下文的类型
+    if (_mime) {
+        ctx.type = _mime
+    }
+
+    // 输出静态资源内容
+    if (_mime && _mime.indexOf('image/') >= 0) {
+        // 如果是图片，则用node原生res，输出二进制数据
+        ctx.res.writeHead(200)
+        ctx.res.write(_content, 'binary')
+        ctx.res.end()
+    } else {
+        // 其他则输出文本
+        ctx.body = _content
+    }
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+app.use(serve(__dirname))
+app.listen(8201, () => {
+    console.log('koa is listening on 8201...');
+})
